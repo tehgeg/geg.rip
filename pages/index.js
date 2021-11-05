87c48e05c1d29a78850d02adf8901714dad311b1
geg <connorcimino1@gmail.com>
2021-11-04 20:38:54 -0400

    Add page and link to paigow page for testing

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -19,6 +20,10 @@ export default function Home() {
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <p className={styles.description}>
+          <Link href="/casino/paigow">Paigow</Link>
         </p>
 
         <div className={styles.grid}>
