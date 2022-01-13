4225b1a7ffb53b8601fb10b6e692d1499047833e
geg <connorcimino1@gmail.com>
2022-01-12 19:36:55 -0500

    WIP: replace classic card assets

--- components/Card.js
+++ components/Card.js
@@ -5,10 +5,10 @@ const Card = ({ card }) => (
   <div className={styles.card_container} key={`${card.value}_${card.suit}`}>
     <div className={styles.card}>
       <div className={styles.card_front}>
-        <Image height={84} width={62} src={`/cards/alt/${card.value}_${card.suit}.png`} alt="" />
+        <Image height={84} width={62} src={`/cards/classic/${card.value}_${card.suit}.png`} alt="" />
       </div>
       <div className={styles.card_back}>
-        <Image height={84} width={62} src={`/cards/alt/card_back.png`} alt="card_back" />
+        <Image height={84} width={62} src={`/cards/classic/card_back.png`} alt="card_back" />
       </div>
     </div>
   </div>
