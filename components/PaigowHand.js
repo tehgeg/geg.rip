09ae6118b24f5e1089bce48adfd3366bdd9986c3
geg <connorcimino1@gmail.com>
2022-03-23 12:51:24 -0400

    Add test hands, styling

--- components/PaigowHand.js
+++ components/PaigowHand.js
@@ -5,42 +5,38 @@ const PaigowHand = ({ hand, deck, set = false }) => (
   (
     set ?
       (
-        <div className={styles.hand}>
-          <>
-            <div className={styles.handFull}>
-              {hand.lowRank}
-              <div className={styles.handCards}>
-                {
-                  hand.low.map((card, i) => (
-                    <Card card={card} deck={deck} key={`$${card.value}_${card.suit}`} />
-                  ))
-                }
-              </div>
+        <>
+          <div className={styles.handFull}>
+            {hand.lowRank}
+            <div className={styles.handCards}>
+              {
+                hand.low.map((card, i) => (
+                  <Card card={card} deck={deck} key={`$${card.value}_${card.suit}`} />
+                ))
+              }
             </div>
-            <div className={styles.spacer} />
-            <div className={styles.handFull}>
-              {hand.highRank}
-              <div className={styles.handCards}>
-                {
-                  hand.high.map((card, i) => (
-                    <Card card={card} deck={deck} key={`$${card.value}_${card.suit}`} />
-                  ))
-                }
-              </div>
+          </div>
+          <div className={styles.spacer} />
+          <div className={styles.handFull}>
+            {hand.highRank}
+            <div className={styles.handCards}>
+              {
+                hand.high.map((card, i) => (
+                  <Card card={card} deck={deck} key={`$${card.value}_${card.suit}`} />
+                ))
+              }
             </div>
-          </>
-        </div>
+          </div>
+        </>
       )
       :
       (
         <div className={styles.hand}>
-          <>
-            {
-              hand.hand.map((card, i) => (
-                <Card card={card} deck={deck} key={`${card.value}_${card.suit}`} />
-              ))
-            }
-          </>
+          {
+            hand.hand.map((card, i) => (
+              <Card card={card} deck={deck} key={`${card.value}_${card.suit}`} />
+            ))
+          }
         </div>
       )
   )
